a4ad414bd6751f498f23bb879d10c2c33fa9383e
kacperkaz1 <kackazmierczak02@protonmail.com>
2022-11-02 11:56:20 +0100

    adding JS

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
+const randomMessages = ["What is fast, loud and crunchy? A rocket chip.", "What is a room with no walls? A mushroom.", "What do you call a guy who's really loud? Mike.", "What's brown and sticky? A stick.", "What do you call a tired bull? A bulldozer."];
 
+const generateRand = randomMessages[Math.floor(Math.random() * randomMessages.length)];
+
+// console.log(generateRand);
