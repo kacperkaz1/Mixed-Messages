ec9f458decb23a6215aac26021755bbe6934752d
Mikolaj Kostecki <kosteckimikolaj2002@gmail.com>
2022-11-06 16:28:19 +0100

    index.html and main.js modified

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const randomMessages = ["What is fast, loud and crunchy? A rocket chip.", "What is a room with no walls? A mushroom.", "What do you call a guy who's really loud? Mike.", "What's brown and sticky? A stick.", "What do you call a tired bull? A bulldozer."];
 
-const generateRand = randomMessages[Math.floor(Math.random() * randomMessages.length)];
+let usedIndex = -1;
 
-// console.log(generateRand);
+function dontRepeat(){
+    let randomIndex = Math.floor(Math.random() * randomMessages.length);
+    while (randomIndex == usedIndex){
+        randomIndex = Math.floor(Math.random() * randomMessages.length);
+    }
+    console.log(randomMessages[randomIndex]);
+    // document.getElementsByClassName("main").innerHTML = randomMessages[randomIndex];
+    usedIndex = randomIndex;
+
+}
+console.log(dontRepeat());
