44c545fc680ff55b490571718836a98dc2af4a13
Mikolaj Kostecki <kosteckimikolaj2002@gmail.com>
2022-11-06 18:40:46 +0100

    script is done, finally

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 const randomMessages = ["What is fast, loud and crunchy? A rocket chip.", "What is a room with no walls? A mushroom.", "What do you call a guy who's really loud? Mike.", "What's brown and sticky? A stick.", "What do you call a tired bull? A bulldozer."];
 
-<<<<<<< HEAD
-let usedIndex = -1;
-
-function dontRepeat(){
-    let randomIndex = Math.floor(Math.random() * randomMessages.length);
-    while (randomIndex == usedIndex){
-        randomIndex = Math.floor(Math.random() * randomMessages.length);
-    }
-    console.log(randomMessages[randomIndex]);
-    // document.getElementsByClassName("main").innerHTML = randomMessages[randomIndex];
-    usedIndex = randomIndex;
-
-}
-console.log(dontRepeat());
-=======
 // const generateRand = randomMessages[Math.floor(Math.random() * randomMessages.length)];
 
 // console.log(generateRand);
@@ -22,7 +7,7 @@ console.log(dontRepeat());
 const btn = document.getElementById("snickers");
 
 
-btn.addEventListener("click", GeneratingMessage, false);
+btn.addEventListener("click", generateRand, false);
 
 // function randomNoRepeats(array) {
 //       var copy = array.slice(0);
@@ -33,23 +18,41 @@ btn.addEventListener("click", GeneratingMessage, false);
 //       return item;
 //     }
 
+// function randomNoRepeats(array) {
+//     var copy = array.slice(0);
+//     return dontRepeat(copy);
+// }
 
-function dontRepeat(copy){
-    if (copy.length < 1) { copy = array.slice(0); }
-    var index = Math.floor(Math.random() * copy.length);
-    var item = copy[index];
-    copy.splice(index, 1);
-    return item;
-  };
+// function dontRepeat(copy){
+//     if (copy.length < 1) { copy = array.slice(0); }
+//     var index = Math.floor(Math.random() * copy.length);
+//     var item = copy[index];
+//     copy.splice(index, 1);
+//     return item;
+// };
 
-  function randomNoRepeats(array) {
-    var copy = array.slice(0);
-    return dontRepeat(copy);
-  }
+// function GeneratingMessage(){
+//     const message = randomNoRepeats(randomMessages);
+//     document.getElementsByClassName("content")[0].innerText = randomNoRepeats(randomMessages);
+// }
 
-  function GeneratingMessage(){
-    const message = randomNoRepeats(randomMessages);
-    document.getElementsByClassName("content")[0].innerText = randomNoRepeats(randomMessages);
+let usedIndexes = [];
+function generateRand(){
+    return displayMessage(Math.floor(Math.random() * randomMessages.length));
 }
 
->>>>>>> b3a1644b892297fa947dd3f9a8a1185526d163df
+function displayMessage(randomIndex){
+    while(true){
+        if (usedIndexes.length == randomMessages.length){
+            usedIndexes = usedIndexes.splice(0,0);
+        }
+        if (!usedIndexes.includes(randomIndex)){
+            document.getElementsByClassName('content')[0].innerText = randomMessages[randomIndex];
+            console.log(randomMessages[randomIndex]);
+            usedIndexes.push(randomIndex);
+            break;
+        }else {
+            randomIndex = Math.floor(Math.random() * randomMessages.length);
+        }
+    }
+}
